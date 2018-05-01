d77f9a4c2298cf8284ae5a7d5083bdb7c68a782f
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-05-01 15:33:02 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+    if message.content.startswith("pls beg"):
+        await client.send_message(message.channel,"pls beg")
+        
+    if message.content.upper().startswith("pls beg"):
+    
+        await client.send_message(message.channel,"pls beg")
+        
+    if message.content.upper().startswith("qolpak"):
+    
+        await client.send_message(message.channel,"qolpak")
+
+    if message.content.upper().startswith("qolpak"):
+    
+        await client.send_message(message.channel,"qolpak")
+
+bot.login(process.env.TOKEN);
