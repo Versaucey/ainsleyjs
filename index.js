5bbbc89991abac5f16861d3b21ea39b2c0beb82c
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-05-01 17:21:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ client.on("message", async message => {
   }
 
 
-bot.user.setActivity('Spamming nimas server') })
+bot.user.setActivity('Spamming nimas server', { type: 'WATCHING' }) })
 
 
 client.login(process.env.B0T_T0KEN);
