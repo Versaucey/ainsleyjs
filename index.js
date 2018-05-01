94696c032af0c2723a325c7beb8838246dba7a14
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-05-01 17:00:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ client.on("message", async message => {
 });
 
 
-client.user.setActivity('Nimas server get spammed', { type: 'WATCHING' })
+bot.user.setActivity('Nimas server get spammed', { type: 'WATCHING' })
 
 
 client.login(process.env.B0T_T0KEN);
